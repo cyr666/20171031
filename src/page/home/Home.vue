ab89b41ac3248a9713459bb8b2c0321e7020bc21
Surielxia <530084905@qq.com>
2017-11-03 13:43:54 +0800

    xia

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,12 +1,15 @@
 <template>
-  <app-header/>
+	<app-header/>
+	<weekendsgo-content/>
 </template>
 
 <script>
-	import HeaderComponent from "./Header"
+	import HeaderComponent from "./Header";
+	import WeekendsgoComponent from "./Weekendsgo";
 	export default {
 	components:{
-		"app-header":HeaderComponent
+		"app-header":HeaderComponent,
+		"weekendsgo-content": WeekendsgoComponent
 	}
 }
 </script>
---
+++ src/page/home/Weekendsgo.vue
@@ -0,0 +1,103 @@
+<template>
+	<div class="weekendsgo">
+		<div class="wg-title">
+			周末去哪儿
+		</div>
+		<div class="wg-class">
+			<img src="./1.jpg" />
+			<div class="wg-words">
+				<p class="w-title">
+					北京温泉排行榜
+				</p>
+				<p class="w-content">
+					细数北京温泉，温暖你的冬天
+				</p>
+			</div>
+		</div>
+		<div class="wg-class">
+			<img src="./2.jpg" />
+			<div class="wg-words">
+				<p class="w-title">
+					寻找北京的皇城范儿
+				</p>
+				<p class="w-content">
+					数百年的宫廷庙宇，至今依旧威严霸气
+				</p>
+			</div>
+		</div>
+		<div class="wg-class">
+			<img src="./3.jpg" />
+			<div class="wg-words">
+				<p class="w-title">
+					北京必游TOP10
+				</p>
+				<p class="w-content">
+					来北京比去的景点非这些地方莫属
+				</p>
+			</div>
+		</div>
+		<div class="wg-class">
+			<img src="./4.jpg" />
+			<div class="wg-words">
+				<p class="w-title">
+					登高望远秋色佳
+				</p>
+				<p class="w-content">
+					山顶满眼彩色，此情此景醉了
+				</p>
+			</div>
+		</div>
+		<div class="wg-class">
+			<img src="./5.jpg" />
+			<div class="wg-words">
+				<p class="w-title">
+					秋高气爽去礼佛
+				</p>
+				<p class="w-content">
+					秋风伴着美妙经声，品尝秋的另一番滋味
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+	.weekendsgo{
+		background: #eee;
+	}
+	.wg-title{
+		height: .88rem;
+		background: #eee;
+		color: #000;
+		font-size: .32rem;
+		line-height: .88rem;
+		text-indent: .3rem;
+	}
+	.wg-class{
+		background: white;
+		margin-top: .08rem;
+	}
+	.wg-class img{
+		height: 2.5rem;
+	}
+	.wg-words{
+		padding: .1rem 0 .26rem;
+	}
+	.w-title{
+		height: .6rem;
+		color: #000;
+		font-size: .32rem;
+		line-height: .6rem;
+		text-indent: .3rem;
+	}
+	.w-content{
+		font-size: .26rem;
+		text-indent: .3rem;
+	}
+</style>
