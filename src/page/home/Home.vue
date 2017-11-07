2fe4b69fdb096ff8f18cf8e45de7573b92c24f10
tianming9606 <tianming9606@163.com>
2017-11-07 21:46:14 +0800

    '201711072146'

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -2,7 +2,7 @@
 	<div>
 		<index-header/>
 		<index-swiper/>
-		<index-iconswiper/>
+		<index-iconswiper :iconswiperInfo="iconswiperInfo" :iconswiperInfoSecond="iconswiperInfoSecond"></index-iconswiper>
 		<index-Activity/>
 		<index-recommend/>
 		<index-weekendsgo/>
@@ -17,32 +17,48 @@
 	import HotRecommend from "./components/Recommend";
 	import iconswiper from "./components/Icon_swiper";
 	import axios from "axios";
+	
 	export default {
-	components:{
-		"index-header":HeaderComponent,
-		"index-weekendsgo": WeekendsgoComponent,
-		"index-swiper":SwiperComponent,
-		"index-recommend":HotRecommend,
-		"index-iconswiper":iconswiper,
-		"index-Activity":Activity
-	},
-	methods:{
-		getHomeData:function() {
-			
+		
+		data() {
+			return {
+				iconswiperInfo: [],
+				iconswiperInfoSecond: []
+			}
+		},
+		
+		components:{
+			"index-header":HeaderComponent,
+			"index-weekendsgo": WeekendsgoComponent,
+			"index-swiper":SwiperComponent,
+			"index-recommend":HotRecommend,
+			"index-iconswiper":iconswiper,
+			"index-Activity":Activity
+		},
+		methods:{
+			getHomeData() {
+				axios.get('/static/index.json')
+					.then(this.handleGetDataSucc.bind(this))
+					.catch(this.handleGetDataErr.bind(this))
+			},
+	
+			handleGetDataSucc(response) {
+				if (response.status === 200) {
+					const {data}  = response.data;
+					this.iconswiperInfo = data.iconswiperInfo;
+					this.iconswiperInfoSecond = data.iconswiperInfoSecond;
+				}
+			},
+	
+			handleGetDataErr(err) {
+				console.log(err);
+			}
+		},
+		mounted:function() {
+			this.getHomeData()
 		}
-	},
-	mounted:function() {
-		this.getHomeData()
-//	  	axios.get('/?ID=12345')
-//		    .then(function (response) {
-//		    console.log(response);
-//		    })
-//		    .catch(function (error) {
-//		    console.log(error);
-//	    });
-
+		
 	}
-}
 </script>
 
 <style>
